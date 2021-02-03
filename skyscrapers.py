1a3b30dcc45bf285134d2d1f7d6ec8db0d3329ec
yarkoslav <romanus004@gmail.com>
2021-02-03 09:27:21 +0200

    Done left_to_right_check func

--- skyscrapers.py
+++ skyscrapers.py
@@ -17,3 +17,25 @@ def read_input(path: str):
             line = line.strip()
             data.append(line)
     return data
+
+
+def left_to_right_check(input_line: str, pivot: int):
+    """
+    Check row-wise visibility from left to right.
+    Return True if number of building from the left-most hint is visible looking to the right,
+    False otherwise.
+
+    input_line - representing board row.
+    pivot - number on the left-most hint of the input_line.
+
+    >>> left_to_right_check("412453*", 4)
+    True
+    >>> left_to_right_check("452453*", 5)
+    False
+    """
+    line = input_line[1:-1]
+    is_visible = True
+    for index in range(pivot-1):
+        if line[index] >= line[pivot-1]:
+            is_visible = False
+    return is_visible
